2c7fce7ac4d6041a0ff1547bc49fdf6b11926e08
JuiceboxJones <jderhalli@gmail.com>
2019-01-29 10:58:04 -0800

    basic api framework

--- scripts/api.js
+++ scripts/api.js
@@ -1,2 +1,57 @@
 'use strict';
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jake-andre/bookmarks';
+const api = (function () {
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jake-andre/bookmarks';
+  
+  function listApiFetch(...args) {
+    let error = false;
+    return fetch(...args)
+      .then(res => {
+        if (!res.ok) {
+          // Valid HTTP response but non-2xx status - let's flag an error!
+          error = true;
+        }
+        // In either case, parse the JSON stream:
+        return res.json();
+      })
+      .then(data => {
+        // If error was flagged, throw an error with the JSON message
+        if (error) throw new Error(data.message);
+        // Otherwise, return the data (all promise chains 
+        // return promise objects)
+        return data;
+      })
+      .catch(err => alert(err.message));
+  }
+
+
+  const getBookmark = function() {
+    const url = `${BASE_URL}`;
+    return listApiFetch(url);
+    //return Promise.resolve('A successful response!');
+  };
+
+  const createBookmark = function(name){
+    const url = `${BASE_URL}`;
+    let error = false;
+    return listApiFetch(url,
+      { method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({name})
+      });
+  };
+
+  const updateBookmark = function(id, updateData){
+    const url = `${BASE_URL}/${id}`;
+    return listApiFetch(url, {
+      method: 'PATCH',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(updateData)
+    });
+  };
+
+  const deleteBookmark = function(id){
+    const url = `${BASE_URL}/${id}`;
+    return listApiFetch(url, {
+      method: 'DELETE'
+    });
+  };
--- scripts/store.js
+++ scripts/store.js
@@ -10,3 +10,55 @@ const STORE = {
   }],
   errors: {title: false, url: false},
   createBookmark: false};
+
+/* global Item api */
+
+// eslint-disable-next-line no-unused-vars
+const store = (function(){
+  
+  const addItem = function(item) {
+    try {
+      this.items.push(item);
+    } catch(e) {
+      console.log(e.message);
+    }
+  };
+
+  const findById = function(id) {
+    return this.items.find(item => item.id === id);
+  };
+
+  const findAndUpdate = function(id, newData){
+    const item = this.items.find(item => item.id === id);
+    Object.assign(item, newData);
+  };
+
+  const findAndDelete = function(id) {
+    this.items = this.items.filter(item => item.id !== id);
+  };
+
+// change to rating filter
+  const toggleCheckedFilter = function() {
+    this.hideCheckedItems = !this.hideCheckedItems;
+  };
+
+  const setItemIsEditing = function(id, isEditing) {
+    const item = this.findById(id);
+    item.isEditing = isEditing;
+  };
+
+
+
+  return {
+    items: [],
+    hideCheckedItems: false,
+    searchTerm: '',
+    findAndUpdate,
+    addItem,
+    findById,
+    findAndDelete,
+    toggleCheckedFilter,
+    setItemIsEditing,
+  };
+  
+}());
